4dcd39039589b4c6c6c280c553754f357fa20401
Saltssaumure <themes@saltssaumure.net>
2023-08-21 06:48:43 +0100

    Update info

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,13 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        /** The official name of your theme. */
-        name: "Logarithm",
-        /** The name of your base scss file. */
-        scss: "xp2",
-        /** The name of your theme repository. */
-        repo: "xp2-discord-theme",
-        /** The version of your theme. */
-        version: "1.0.0"
+        name: "Logarithm" /* The official name of your theme. */,
+        scss: "xp2" /* The name of your base scss file. */,
+        repo: "xp2-discord-theme" /* The name of your theme repository. */,
+        version: "1.0.0" /* The version of your theme. */
     }
 };
